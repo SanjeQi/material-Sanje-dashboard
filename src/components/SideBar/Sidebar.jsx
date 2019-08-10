69436eb4ccc10b14ec2565ce5a1fced543eefc01
github username <cristi.nord@gmail.com>
2019-08-10 23:09:57 +0100

    Sidebar & App comp

--- src/components/SideBar/Sidebar.jsx
+++ src/components/SideBar/Sidebar.jsx
@@ -1,8 +1,33 @@
 import React from "react";
+import { List, ListItem, ListItemIcon, ListItemText } from "material-ui";
+import { NavLink } from "react-router-dom";
+
+import appRoutes from "routes/app.jsx";
 
 class Sidebar extends React.Component {
   render() {
-    return <div>salut din Sidebar</div>;
+    return (
+      <List>
+        {appRoutes.map((prop, key) => {
+          if (prop.redirect) return null;
+          return (
+            <NavLink
+              to={prop.path}
+              className="nav-link"
+              activeClassName="active"
+              key={key}
+            >
+              <ListItem button>
+                <ListItemIcon>
+                  <prop.icon />
+                </ListItemIcon>
+                <ListItemText primary={prop.sidebarName} />
+              </ListItem>
+            </NavLink>
+          );
+        })}
+      </List>
+    );
   }
 }
 
