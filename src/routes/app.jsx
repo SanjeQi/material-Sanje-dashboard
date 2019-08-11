d5754f92d2a9bcf767eafd74c0449c9da515810d
github username <cristi.nord@gmail.com>
2019-08-11 22:09:49 +0100

    Routes & Dashboard

--- src/routes/app.jsx
+++ src/routes/app.jsx
@@ -1,60 +1,70 @@
-import Dashboard from "views/Dashboard/Dashboard.jsx";
+import DashboardPage from "views/Dashboard/Dashboard.jsx";
 import UserProfile from "views/UserProfile/UserProfile.jsx";
 import TableList from "views/TableList/TableList.jsx";
 import Typography from "views/Typography/Typography.jsx";
 import Icons from "views/Icons/Icons.jsx";
 import Maps from "views/Maps/Maps.jsx";
-import Notifications from "views/Notifications/Notifications.jsx";
+import NotificationsPage from "views/Notifications/Notifications.jsx";
+
+import {
+  Dashboard,
+  Person,
+  ContentPaste,
+  LibraryBooks,
+  BubbleChart,
+  LocationOn,
+  Notifications
+} from "material-ui-icons";
 
 const appRoutes = [
   {
     path: "/dashboard",
     sidebarName: "Dashboard",
     navbarName: "Material Dashboard",
-    icon: "dashboard",
-    component: Dashboard
+    icon: Dashboard,
+    component: DashboardPage
   },
   {
     path: "/user",
     sidebarName: "User Profile",
     navbarName: "Profile",
-    icon: "person",
+    icon: Person,
     component: UserProfile
   },
   {
     path: "/table",
     sidebarName: "Table List",
     navbarName: "Table List",
-    icon: "content_paste",
+    icon: ContentPaste,
     component: TableList
   },
   {
     path: "/typography",
     sidebarName: "Typography",
     navbarName: "Typography",
-    icon: "library_books",
+    icon: LibraryBooks,
     component: Typography
   },
   {
     path: "/icons",
     sidebarName: "Icons",
     navbarName: "Icons",
-    icon: "bubble_chart",
+    icon: BubbleChart,
     component: Icons
   },
   {
     path: "/maps",
     sidebarName: "Maps",
     navbarName: "Map",
-    icon: "location_on",
+    icon: LocationOn,
     component: Maps
   },
   {
     path: "/notifications",
     sidebarName: "Notifications",
     navbarName: "Notifications",
-    icon: "notifications",
-    component: Notifications
+    icon: Notifications,
+    component: NotificationsPage
   },
   { redirect: true, path: "/", to: "/dashboard" }
 ];
