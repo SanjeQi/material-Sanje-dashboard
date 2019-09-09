1f078889fce08441f02c2a4a4d355ab422df88f0
github username <cristi.nord@gmail.com>
2019-09-09 16:56:49 +0100

    Typography components H4-H6. Info and Muted comp

---
+++ src/components/Typography/Info.jsx
@@ -0,0 +1,9 @@
+import React from "react";
+
+class Info extends React.Component {
+  render() {
+    return <div>salut din Info</div>;
+  }
+}
+
+export default Info;
---
+++ src/components/Typography/Muted.jsx
@@ -0,0 +1,9 @@
+import React from "react";
+
+class Muted extends React.Component {
+  render() {
+    return <div>salut din Muted</div>;
+  }
+}
+
+export default Muted;
